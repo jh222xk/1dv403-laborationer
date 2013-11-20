fa768eaac132bbab6f283dd14101be01154a4d22
Jesper Håkansson <drager@programmers.se>
2013-11-20 15:40:05 +0100

    Completed the last one

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,9 +1,37 @@
 "use strict";
 
-var makePerson = function(persArr){
+var makePerson = function(persArr) {
+    var nameArr = [],
+        ageArr = [];
 
+    // Append the name and age once per array element.
+    persArr.forEach(function (persArr) {
+        nameArr.push(persArr.name);
+        ageArr.push(persArr.age);
+    });
 
-	// Din kod här...
+    // Sort the nameArr and join all elements 
+    // of the array into a string with a comma and a space.
+    nameArr = nameArr.sort(function(a, b) {
+        return a.localeCompare(b);
+    }).join(', ');
 
-}
+    // Reduce each value of array to a single value and divide it
+    // with the lenght of the ageArr (3).
+    var result = {
+        minAge: Math.min.apply(null, ageArr),
+        maxAge: Math.max.apply(null, ageArr),
+        averageAge: Math.round((ageArr.reduce(function(a, b) {
+            return a + b;
+        }, 0)) / ageArr.length),
+        names: nameArr
+    };
 
+    return result;
+};
+
+var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+
+var result = makePerson(data);
+
+console.log(result);
