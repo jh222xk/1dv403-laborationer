b22fcb17a0a13de0d2fe5f139d07b2dc0e759264
Jesper Håkansson <drager@programmers.se>
2013-11-20 21:05:00 +0100

    Complted the third one

--- 1-vandalen/birthday/script.js
+++ 1-vandalen/birthday/script.js
@@ -4,14 +4,29 @@ window.onload = function(){
 
 	
 	var birthday = function(date){
-		
+		// Check if date is formatted as; YYYY-MM-DD.
+		if (!date.match(/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/)) {
+			throw new Error("Felformaterat datum! Ange datum enligt: YYYY-MM-DD ");
+		}
 
+		// Create a date object from the current date.
+		var dateNow = new Date(),
+			year = dateNow.getFullYear();
 
-			// Din kod här.
+		// Create a new date object from the date parameter.
+		date = new Date(date);
+		date.setDate(date.getDate() + 1);
 
+		// Set the year to the current year (2013).
+		date.setFullYear(year);
 
+		// The diffrence in ms.
+		var difference = date.getTime() - dateNow.getTime();
 
+		// Divide the diffrence with one day in ms.
+		var days = Math.round((difference)/(1000 * 60 * 60 * 24));
 
+		return days;
 	};
 	// ------------------------------------------------------------------------------
 
@@ -28,7 +43,7 @@ window.onload = function(){
 		p.classList.remove( "error");
 
 		try {
-			var answer = birthday(input.value) // Läser in texten från textrutan och skickar till funktionen "convertString"
+			var answer = birthday(input.value); // Läser in texten från textrutan och skickar till funktionen "convertString"
 			var message;
 			switch (answer){
 				case 0: message = "Grattis på födelsedagen!";
