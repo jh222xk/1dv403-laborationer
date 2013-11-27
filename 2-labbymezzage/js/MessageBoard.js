85b6d1ad212503dc89b22e6325d42c80b4b1b7bd
Jesper Håkansson <jh222xk@student.lnu.se>
2013-11-27 10:17:08 +0100

    Added getters and setters as well as some other methods

--- 2-labbymezzage/js/MessageBoard.js
+++ 2-labbymezzage/js/MessageBoard.js
@@ -1,8 +1,22 @@
+"use strict";
+
 var MessageBoard = {
 
+   messages: [],
+
     init: function() {
+        var mess = new Message("Hello World!", new Date());
+        alert(mess);
+        alert(mess.getText());
+        mess.setText("Foo bar");
+        alert(mess);
+        mess.setDate(new Date(111111111111111));
+        alert(mess);
+
+
     }
     
 };
 
-MessageBoard.init(alert("Hello World!"));
+window.onload = MessageBoard.init;
+
