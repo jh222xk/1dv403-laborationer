f6616444529eabc28de27ea2f3da36d86dacae36
Jesper Håkansson <jh222xk@student.lnu.se>
2013-12-02 20:20:31 +0100

    Added counter for all the messages for the init

--- 2-labbymezzage/js/MessageBoard.js
+++ 2-labbymezzage/js/MessageBoard.js
@@ -2,21 +2,24 @@
 
 var MessageBoard = {
 
-   messages: [],
+    messages: [],
 
     init: function() {
-        var mess = new Message("Hello World!", new Date());
-        alert(mess);
-        alert(mess.getText());
-        mess.setText("Foo bar");
-        alert(mess);
-        mess.setDate(new Date(111111111111111));
-        alert(mess);
+        // Get some element as well as create the div for the count.
+        var messageWrap = document.querySelector(".message-wrap"),
+            textarea = document.querySelector("#id_bodytext"),
+            submitButton = document.querySelector("#submit"),
+            messageCount = document.createElement("div");
 
+        // Add an id to the message count.
+        // Set the innerHTML to what the messages array length is. 
+        messageCount.setAttribute("id", "message-count");
+        messageCount.innerHTML = "Antal meddelanden: " + MessageBoard.messages.length;
 
+        messageWrap.appendChild(messageCount);
+
+        // Set focus on the textarea when the page is done rendering.
+        textarea.focus();
     }
-    
 };
-
 window.onload = MessageBoard.init;
-
