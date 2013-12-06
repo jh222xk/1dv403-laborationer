749e7e83972c4e87b623d968c87125a1d6535fe6
Jesper Håkansson <jh222xk@student.lnu.se>
2013-12-06 11:09:39 +0100

    Added so you can 'flip bricks'

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -17,6 +17,7 @@ var Memory = {
         var currentImg = 0;
         this.random = RandomGenerator.getPictureArray(rows, cols);
         var wrapperElement = document.querySelector(".memory-wrap");
+        var imageLink = document.getElementsByTagName("a");
 
         // Iterate through the number of rows, create it and append it.
         for (var row = rows - 1; row >= 0; row--) {
@@ -44,7 +45,74 @@ var Memory = {
                 currentImg += 1;
             }
         }
-    }
+
+        // Iterate through all the anchortags on the page
+        // and when the user clicks on an image call the
+        // turnBrick function.
+        for (var i = 0; i < imageLink.length; i++) {
+            imageLink[i].onclick = function() {
+                self.turnBrick(this);
+                return false;
+            };
+        }
+    },
+
+    turnBrick: function (card) {
+        // If brickOne and brickTwo isn't null just return so 
+        // the user can't turn more bricks
+        if (this.brickOne !== null && this.brickTwo !== null) {
+            return;
+        }
+        console.log(this);
+        var activeCard;
+        var img = card.querySelector("img");
+
+        // Set the source of the image to what the card id is.
+        img.setAttribute("src", "pics/"+ this.random[card.id] + ".png");
+
+        if (!this.brickOne) {
+            img.classList.toggle("active-card");
+            this.brickOne = img;
+        }
+        else if (!this.brickTwo) {
+            img.classList.toggle("active-card");
+            this.brickTwo = img;
+            this.checkCards();
+        }
+    },
+
+    checkCards: function () {
+        var self = this;
+
+        this.numOfTries += 1;
+
+        // Check the sources is the same.
+        if (this.brickOne.getAttribute("src") === this.brickTwo.getAttribute("src")) {
+            // Add the class completed to those bricks.
+            this.brickOne.setAttribute("class", "completed");
+            this.brickTwo.setAttribute("class", "completed");
+
+            // Set both bricks to null so we can turn some other bricks.
+            this.brickOne = null;
+            this.brickTwo = null;
+            this.completedCards += 1;
+        }
+        else {
+            // Set a timeout to 1000 ms.
+            setTimeout(function() {
+                self.brickOne.classList.toggle("active-card");
+                self.brickTwo.classList.toggle("active-card");
+
+                // "Flip the brick back".
+                self.brickOne.setAttribute("src", "pics/"+ 0 + ".png");
+                self.brickTwo.setAttribute("src", "pics/"+ 0 + ".png");
+
+                // Set both bricks to null so we can turn some other bricks.
+                self.brickOne = null;
+                self.brickTwo = null;
+            }, 1000);
+        }
+    },
 };
 
 window.onload = Memory.init;
